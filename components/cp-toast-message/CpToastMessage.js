27ebb831a6d2559bb74fd1da0e5c7efc0a06ef58
mansa0528 <manasa.akula@merkle.com>
2025-09-10 15:10:08 +0530

    All Popup and notification - UI

--- components/cp-toast-message/CpToastMessage.js
+++ components/cp-toast-message/CpToastMessage.js
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import Image from "next/image";
 
 export default function ToastMessage({
+  icon,
   message,
   show,
   onClose,
@@ -21,18 +22,22 @@ export default function ToastMessage({
   if (!show) return null;
 
   return (
-    <div className="flex flex-col items-end justify-center 
-    fixed bottom-6 bg-[#FFFFFF] left-6 z-50 px-4 py-2 min-w-[346px]
-    rounded-lg shadow-lg animate-slide-in border-1 border-[#EEEEF1]">
-        <button className="text-gray-400 hover:text-gray-600">
-            <Image src="/assets/images/close.png" alt="Icon" width={14} height={14} />
-        </button>
-      <div className="flex items-center justify-center gap-3">
-        <Image src="/assets/images/tick-icon.png" alt="Icon" width={20} height={20} />
-      <div className="text-[#0D0D11]">
-        {message}
+  <div className="flex items-center justify-between rounded-md bg-[#FFFFFF] fixed bottom-6 left-6 z-50 px-1 w-full max-w-[345px] shadow-md border-1 border-[#EEEEF1]">
+    <div className="flex items-start justify-center space-x-2 py-1">
+      <div className="w-[2px] self-stretch bg-[#022B59] rounded-[3px]"></div>
+      <div className="flex items-center justify-center mt-[2px] py-2 pl-2">
+        {icon}
+      </div>
+      <div className="flex flex-col items-start py-2">
+      <div className="text-[14px] text-[#0D0D11] font-bold">Toast Heading</div>
+      <div className="text-[14px] text-[#0D0D11]">{message}</div>
+      <button className="text-sm text-[#022B59] underline decoration-[#022B59] mt-1 font-semibold">BUTTON</button>
       </div>
     </div>
-    </div>
+
+    <button className="text-black text-lg font-semibold hover:text-red-600">
+      <Image src="/assets/images/close.png" alt="Icon" width={14} height={14} className="absolute right-2 top-2"/>
+    </button>
+  </div>
   );
 }
