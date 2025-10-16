a4a8ff75d9c59b91a1c77df3aba36ba2322fe273
mansa0528 <manasa.akula@merkle.com>
2025-10-16 13:26:37 +0530

    New user error msg

---
+++ components/cp-product-details/cp-product-details.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+export default function CpProductDetails() {
+    return(
+        <section className="bg-[url(/assets/images/product-details-bg.png)] bg-cover bg-center h-screen">
+            <div>
+                <div></div>
+            </div>
+        </section>
+    )
+}
