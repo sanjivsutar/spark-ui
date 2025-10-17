ace582232bea40659590574d50693fccae6f2a4c
mansa0528 <manasa.akula@merkle.com>
2025-10-17 15:37:27 +0530

    Creatives tab modified

--- components/cp-client-marketing-strategy/cp-client-marketing-strategy.js
+++ components/cp-client-marketing-strategy/cp-client-marketing-strategy.js
@@ -123,7 +123,6 @@ export default function CpClientMarketingStrategy() {
                         </button>
                     </div>
                 </div>
-
             )}
         </div>
     );
